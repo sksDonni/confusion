56e84fe374ad7edb1da71c3bea34ec1d15ca89d7
suchith Krishna S Donni <sksdonni18@gmail.com>
2021-09-19 23:56:16 +0530

    contact made class component

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -1,6 +1,51 @@
 import React from "react";
-import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import React, { Component } from 'react';
+import { Breadcrumb, BreadcrumbItem,
+            Button, Form, FormGroup, Label, Input, Col } from 'reactstrap';
 import { Link } from "react-router-dom";
+
+class Contact extends Component {
+  constructor(props) {
+          super(props);
+
+          this.state = {
+              firstname: '',
+              lastname: '',
+              telnum: '',
+              email: '',
+              agree: false,
+              contactType: 'Tel.',
+              message: ''
+          };
+
+          this.handleInputChange = this.handleInputChange.bind(this);
+          this.handleSubmit = this.handleSubmit.bind(this);
+          
+      }
+
+      handleInputChange(event) {
+          const target = event.target;
+          const value = target.type === 'checkbox' ? target.checked : target.value;
+          const name = target.name;
+      
+          this.setState({
+            [name]: value
+          });
+      }
+
+      handleSubmit(event) {
+          console.log('Current State is: ' + JSON.stringify(this.state));
+          alert('Current State is: ' + JSON.stringify(this.state));
+          event.preventDefault();
+      }
+
+  
+  render() {    
+  }
+
+}
+
+
 
 function Contact(props) {
   return (
