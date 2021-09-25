dc61a0b24b988ad1845db9c448688ea80b9fd646
suchith Krishna S Donni <sksdonni18@gmail.com>
2021-09-25 23:52:47 +0530

    action creator

--- src/redux/Comments.js
+++ src/redux/Comments.js
@@ -1,8 +1,16 @@
 import {COMMENTS} from '../comments'
+import * as ActionTypes from './ActionTypes'
 
 export const Comments = (state = COMMENTS, action) => {
 	switch(action.type)
 	{
+		case ActionTypes.ADD_COMMENT:
+			let comment = action.payload
+			comment.id = state.length
+			comment.date = new Date().toISOString()
+			console.log(comment);
+			return state.concat(comment)
+
 		default:
 			return state;
 	}
