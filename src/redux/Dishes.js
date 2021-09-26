d5b2fddd254b81fb084ac57d4dc77a26d098afc7
suchith Krishna S Donni <sksdonni18@gmail.com>
2021-09-26 23:50:36 +0530

    dummy

--- src/redux/Dishes.js
+++ src/redux/Dishes.js
@@ -3,6 +3,8 @@ import {DISHES} from '../dishes'
 export const Dishes = (state = DISHES, action) => {
 	switch(action.type)
 	{
+		case'ADD_DISHES':
+			return state;
 		default:
 			return state;
 	}
