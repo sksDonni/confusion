b989bac69a731a0c43cec7c1a2d032c4cee47a6c
suchith Krishna S Donni <sksdonni18@gmail.com>
2021-10-19 22:02:20 +0530

    assignment 2 server files

---
+++ confusionServer/models/leaders.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+require('mongoose-currency').loadType(mongoose);
+const Currency = mongoose.Types.Currency;
+
+const Leaders = new Schema({
+	name:{
+		type: String,
+		required: true,
+		unique: true
+	},
+	image:{
+		type: String,
+		required: true
+	},
+	designation:{
+		type: String,
+		required: true
+	},
+	abbr:{
+		type: String,
+		default:''
+	},
+	description:{
+		type: String,
+		required: true
+	},
+	featured:{
+		type: Boolean,
+		default: false
+	}
+
+})
